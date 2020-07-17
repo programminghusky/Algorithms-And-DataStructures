29344790cc19b3382b688165c2bb92c5ca6e651d
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-07-17 11:11:03 -0700

    Missing number using XOR

---
+++ InterviewPrepQuestions/BitwiseXOR/MissingNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.BitwiseXOR
+{
+    class MissingNumber
+    {
+        public int FindMissingNumber(int[] arr)
+        {
+            int n = arr.Length;
+            int xor = 0;
+            for (int i = 0; i < n ; i++)
+            {
+                int missNum = i + 1;
+                xor = xor ^ arr[i] ^ missNum;
+            }
+
+            xor = xor ^ (n + 1);
+            return xor;
+        }
+    }
+}
--- InterviewPrepQuestions/Program.cs
+++ InterviewPrepQuestions/Program.cs
@@ -10,8 +10,11 @@ namespace InterviewPrepQuestions
     {
         static void Main(string[] args) 
         {
-            TwoPointers.SortedArraySquares sorted = new TwoPointers.SortedArraySquares();
-            var arr = sorted.MakeSquares(new int[] { -3, -1, 0, 1, 2 });
+            //TwoPointers.SortedArraySquares sorted = new TwoPointers.SortedArraySquares();
+            //var arr = sorted.MakeSquares(new int[] { -3, -1, 0, 1, 2 });
+
+            BitwiseXOR.MissingNumber num = new BitwiseXOR.MissingNumber();
+            var n = num.FindMissingNumber(new int[] { 1, 5, 2, 6, 4 });
             Console.ReadLine();
         }
     }
