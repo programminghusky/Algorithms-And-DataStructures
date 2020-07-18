a536c811b57888b500a1ad2b24bbf21058b817bb
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-07-18 11:11:39 -0700

    LinkedList has cycle

---
+++ InterviewPrepQuestions/FastAndSlowPointers/LinkedListCycle.cs
@@ -0,0 +1,38 @@
+﻿/* Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.FastAndSlowPointers
+{
+    class ListNode
+    {
+        int Value = 0;
+        public ListNode Next;
+
+        public ListNode(int value)
+        {
+            Value = value;
+        }
+    }
+
+    class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while(fast !=null && fast.Next!=null)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+                if (slow == fast)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
--- InterviewPrepQuestions/Program.cs
+++ InterviewPrepQuestions/Program.cs
@@ -13,8 +13,21 @@ namespace InterviewPrepQuestions
             //TwoPointers.SortedArraySquares sorted = new TwoPointers.SortedArraySquares();
             //var arr = sorted.MakeSquares(new int[] { -3, -1, 0, 1, 2 });
 
-            BitwiseXOR.MissingSingleNumber num = new BitwiseXOR.MissingSingleNumber();
-            var n = num.SingleNumber(new int[] { 1, 4, 2, 1, 3, 2, 3 });
+            //BitwiseXOR.MissingSingleNumber num = new BitwiseXOR.MissingSingleNumber();
+            //var n = num.SingleNumber(new int[] { 1, 4, 2, 1, 3, 2, 3 });
+
+        
+            FastAndSlowPointers.LinkedListCycle cycle = new FastAndSlowPointers.LinkedListCycle();
+
+            FastAndSlowPointers.ListNode head = new FastAndSlowPointers.ListNode(1);
+            head.Next = new FastAndSlowPointers.ListNode(2);
+            head.Next.Next = new FastAndSlowPointers.ListNode(3);
+            head.Next.Next.Next = new FastAndSlowPointers.ListNode(4);
+            head.Next.Next.Next.Next = new FastAndSlowPointers.ListNode(5);
+            head.Next.Next.Next.Next.Next = new FastAndSlowPointers.ListNode(6);
+            head.Next.Next.Next.Next.Next.Next = head.Next.Next;
+            bool hasCycle = cycle.HasCycle(head);
+
             Console.ReadLine();
         }
     }
