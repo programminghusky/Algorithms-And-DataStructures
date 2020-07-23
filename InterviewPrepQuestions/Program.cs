6d4307a8173464b293b80924441a0f787f40e9f5
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-07-23 14:44:59 -0700

    No repeat substring

--- InterviewPrepQuestions/Program.cs
+++ InterviewPrepQuestions/Program.cs
@@ -16,19 +16,20 @@ namespace InterviewPrepQuestions
             //BitwiseXOR.MissingSingleNumber num = new BitwiseXOR.MissingSingleNumber();
             //var n = num.SingleNumber(new int[] { 1, 4, 2, 1, 3, 2, 3 });
 
-        
-            FastAndSlowPointers.RearrangeLinkedList rearrange = new FastAndSlowPointers.RearrangeLinkedList();
 
-            FastAndSlowPointers.ListNode head = new FastAndSlowPointers.ListNode(1);
-            head.Next = new FastAndSlowPointers.ListNode(2);
-            head.Next.Next = new FastAndSlowPointers.ListNode(3);
-            head.Next.Next.Next = new FastAndSlowPointers.ListNode(4);
+            //FastAndSlowPointers.RearrangeLinkedList rearrange = new FastAndSlowPointers.RearrangeLinkedList();
+
+            //FastAndSlowPointers.ListNode head = new FastAndSlowPointers.ListNode(1);
+            //head.Next = new FastAndSlowPointers.ListNode(2);
+            //head.Next.Next = new FastAndSlowPointers.ListNode(3);
+            //head.Next.Next.Next = new FastAndSlowPointers.ListNode(4);
             //head.Next.Next.Next.Next = new FastAndSlowPointers.ListNode(10);
             //head.Next.Next.Next.Next.Next = new FastAndSlowPointers.ListNode(12);
             ////head.Next.Next.Next.Next.Next.Next = head.Next.Next;
             //var node = cycle.FindMiddle(head);
 
-            rearrange.ReorderList(head);
+            SlidingWindow.NoRepeatSubstring str = new SlidingWindow.NoRepeatSubstring();
+            int length = str.LengthOfLongestSubstring("pwwkew");
 
             Console.ReadLine();
         }
---
+++ InterviewPrepQuestions/SlidingWindow/NoRepeatSubstring.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.SlidingWindow
+{
+    class NoRepeatSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int maxLength = 0, index = 0, j = 0;
+            HashSet<char> charSet = new HashSet<char>();
+
+            while(j < s.Length)
+            {
+                if(! charSet.Contains(s[j]))
+                {
+                    charSet.Add(s[j]);
+                    j++;
+                    maxLength = Math.Max(maxLength, j - index);
+                }
+                else
+                {
+                    charSet.Remove(s[index]);
+                    index++;
+                }
+            }
+            return maxLength;
+        }
+            /* Brute Force 
+            public int LengthOfLongestSubstring(string s)
+            {
+                string str = s[0] + "";
+                int length = 1;
+                int maxLength = int.MinValue;
+
+                for(int i = 1; i < s.Length; i++)
+                {
+                    if(!str.Contains(s[i]))
+                    {
+                        str = str + s[i];
+                        length++;
+                        maxLength = Math.Max(length, maxLength);
+                    }
+                    else
+                    {
+                        while(str.Contains(s[i]))
+                        {
+                            str = str.Substring(1);
+                            length--;
+                        }
+                        str = str + s[i];
+                        length++;
+                    }
+                }
+                return maxLength;
+            }
+            */
+        }
+}
