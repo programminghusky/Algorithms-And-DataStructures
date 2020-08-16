024127982ad016ab138dc091557b26d6dc2d989d
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-08-16 12:20:01 -0700

    Binary Tree Reverse Level Order Traversal

--- InterviewPrepQuestions/TreeBreadthFirstSearch/BinaryTreeReverseLevelOrderTraversal.cs
+++ InterviewPrepQuestions/TreeBreadthFirstSearch/BinaryTreeReverseLevelOrderTraversal.cs
@@ -1,6 +1,4 @@
-﻿/* Given a binary tree, populate an array to represent its level-by-level traversal. 
- * You should populate the values of all nodes of each level from left to right in separate sub-arrays. */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +6,9 @@ using System.Threading.Tasks;
 
 namespace InterviewPrepQuestions.TreeBreadthFirstSearch
 {
-    class TreeNode
+    class BinaryTreeReverseLevelOrderTraversal
     {
-        public TreeNode left;
-        public TreeNode right;
-        public int val;
-        public TreeNode(int val)
-        {
-            this.val = val;
-        }
-    }
-    class BinaryTreeLevelOrderTraversal
-    {
-        public IList<IList<int>> LevelOrder(TreeNode root)
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
         {
             IList<IList<int>> listOfNodes = new List<IList<int>>();
             if (root == null)
@@ -33,20 +21,20 @@ namespace InterviewPrepQuestions.TreeBreadthFirstSearch
             {
                 int length = nodes.Count;
                 levelList = new List<int>();
-                for(int i = 0; i < length; i++)
+                for (int i = 0; i < length; i++)
                 {
                     TreeNode node = nodes.Dequeue();
                     levelList.Add(node.val);
-                    if(node.left != null)
+                    if (node.left != null)
                     {
                         nodes.Enqueue(node.left);
                     }
-                    if(node.right != null)
+                    if (node.right != null)
                     {
                         nodes.Enqueue(node.right);
                     }
                 }
-                listOfNodes.Add(levelList);
+                listOfNodes.Insert(0,levelList);                
             }
             return listOfNodes;
         }
