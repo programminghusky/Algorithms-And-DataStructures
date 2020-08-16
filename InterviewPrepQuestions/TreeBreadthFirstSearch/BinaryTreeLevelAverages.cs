691557c26b0d5c65a032409ff1b610495485d733
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-08-16 12:26:58 -0700

    Binary Tree Level Average

---
+++ InterviewPrepQuestions/TreeBreadthFirstSearch/BinaryTreeLevelAverages.cs
@@ -0,0 +1,41 @@
+﻿/* Given a binary tree, populate an array to represent the averages of all of its levels. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.TreeBreadthFirstSearch
+{
+    class BinaryTreeLevelAverages
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            IList<double> avgs = new List<double>();
+            if (root == null)
+                return avgs;
+            Queue<TreeNode> qNodes = new Queue<TreeNode>();
+            qNodes.Enqueue(root);
+            while (qNodes.Count != 0)
+            {
+                List<int> intList = new List<int>();
+                int length = qNodes.Count;
+                for (int i = 0; i < length; i++)
+                {
+                    TreeNode node = qNodes.Dequeue();
+                    intList.Add(node.val);
+                    if (node.left != null)
+                        qNodes.Enqueue(node.left);
+                    if (node.right != null)
+                        qNodes.Enqueue(node.right);
+                }
+                double sum = 0;
+                foreach (int val in intList)
+                    sum += val;
+
+                avgs.Add(sum / intList.Count);
+            }
+            return avgs;
+        }
+    }
+}
---
+++ InterviewPrepQuestions/TreeBreadthFirstSearch/BinaryTreeMinimumDepth.cs
@@ -0,0 +1,15 @@
+﻿/* Find the minimum depth of a binary tree. The minimum depth is the number of nodes along the shortest path from the root node
+ * to the nearest leaf node. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.TreeBreadthFirstSearch
+{
+    class BinaryTreeMinimumDepth
+    {
+
+    }
+}
