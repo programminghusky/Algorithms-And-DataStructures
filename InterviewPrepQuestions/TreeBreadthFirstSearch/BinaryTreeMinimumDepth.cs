191127593b951d11eba82d848a2cc099226775cc
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-08-16 12:28:47 -0700

    Binary Tree Minimum Depth

--- InterviewPrepQuestions/TreeBreadthFirstSearch/BinaryTreeMinimumDepth.cs
+++ InterviewPrepQuestions/TreeBreadthFirstSearch/BinaryTreeMinimumDepth.cs
@@ -10,6 +10,16 @@ namespace InterviewPrepQuestions.TreeBreadthFirstSearch
 {
     class BinaryTreeMinimumDepth
     {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            if (root.left == null)
+                return MinDepth(root.right) + 1;
+            if (root.right == null)
+                return MinDepth(root.left) + 1;
 
+            return Math.Min((1 + MinDepth(root.left)), (1 + MinDepth(root.right)));
+        }
     }
 }
