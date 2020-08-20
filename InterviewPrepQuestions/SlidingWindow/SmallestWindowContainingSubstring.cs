b0f34d2b10bcc5593f8f759bb6ebddd57a2462ed
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-08-20 14:53:56 -0700

    starting indices of substring that are a concatenation of all the given words

---
+++ InterviewPrepQuestions/SlidingWindow/SmallestWindowContainingSubstring.cs
@@ -0,0 +1,64 @@
+﻿/* Given a string and a pattern, find the smallest substring 
+ * in the given string which has all the characters of the given pattern. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.SlidingWindow
+{
+    class SmallestWindowContainingSubstring
+    {
+        public string MinWindow(string s, string t)
+        {
+            int windowStart = 0, matched = 0;
+
+            Dictionary<char, int> charFreqMap = new Dictionary<char, int>();
+            foreach (char ch in t)
+            {
+                if (charFreqMap.ContainsKey(ch))
+                {
+                    charFreqMap[ch]++;
+                }
+                else
+                {
+                    charFreqMap.Add(ch, 1);
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char rightChar = s[i];
+                if (charFreqMap.ContainsKey(rightChar))
+                {
+                    charFreqMap[rightChar]--;
+
+                    if (charFreqMap[rightChar] >= 0)
+                    {
+                        matched++;
+                    }
+                }
+
+                if (matched == charFreqMap.Count)
+                {
+                    return true;
+                }
+
+                if (i >= t.Length - 1)
+                {
+                    char leftChar = s[windowStart];
+                    windowStart++;
+                    if (charFreqMap.ContainsKey(leftChar))
+                    {
+                        if (charFreqMap[leftChar] == 0)
+                            matched--;
+
+                        charFreqMap[leftChar]++;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ InterviewPrepQuestions/SlidingWindow/WordsConcatenation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.SlidingWindow
+{
+    class WordsConcatenation
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            Dictionary<string, int> wordFreqMap = new Dictionary<string, int>();
+            foreach(string word in words)
+            {
+                if(wordFreqMap.ContainsKey(word))
+                {
+                    wordFreqMap[word]++;
+                }
+                else
+                {
+                    wordFreqMap.Add(word, 1);
+                }
+            }
+
+            IList<int> indices = new List<int>();
+            int wordsCount = words.Length;
+            if (wordsCount < 1)
+                return indices;
+            int eachWordLength = words[0].Length;
+
+            for(int i = 0; i <= s.Length - (wordsCount * eachWordLength); i++)
+            {
+                Dictionary<string, int> seen = new Dictionary<string, int>();
+
+                for(int j = 0; j < wordsCount; j++)
+                {
+                    int nextWordIndex = i+ (j * eachWordLength);
+                    string word = s.Substring(nextWordIndex, eachWordLength);
+
+                    if (!wordFreqMap.ContainsKey(word))
+                        break;
+                    if (seen.ContainsKey(word))
+                        seen[word]++;
+                    else
+                        seen.Add(word, 1);
+
+                    if (seen[word] > wordFreqMap[word])
+                        break;
+
+                    if ((j + 1) == wordsCount)
+                        indices.Add(i);
+                }
+            }
+            return indices;
+        }
+    }
+}
