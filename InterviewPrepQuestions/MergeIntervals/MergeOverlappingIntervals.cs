af066142569eb4c00d67c7e2669cc3237ec5caa4
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-08-28 15:00:44 -0700

    Reverse Single Linked List

---
+++ InterviewPrepQuestions/MergeIntervals/MergeOverlappingIntervals.cs
@@ -0,0 +1,19 @@
+﻿/* Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.MergeIntervals
+{
+    class MergeOverlappingIntervals
+    {
+        //public int[][] Merge(int[][] intervals)
+        //{
+        //    if (intervals.Length < 2)
+        //        return intervals;
+
+        //}
+    }
+}
