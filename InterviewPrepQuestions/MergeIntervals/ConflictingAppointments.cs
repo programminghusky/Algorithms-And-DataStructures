6ee078c8bf7af0aa90ebde95cd1ba7b84e152fbf
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-09-08 21:25:53 -0700

    Conflicting Appointments

---
+++ InterviewPrepQuestions/MergeIntervals/ConflictingAppointments.cs
@@ -0,0 +1,31 @@
+﻿/* Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.MergeIntervals
+{
+    class ConflictingAppointments
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            Array.Sort(intervals, new ArrayComparer());
+            for (int i = 0; i < intervals.Length - 1; i++)
+            {
+                if (intervals[i][1] > intervals[i + 1][0])
+                    return false;
+            }
+            return true;
+        }
+
+        class ArrayComparer : IComparer<int[]>
+        {
+            public int Compare(int[] x, int[] y)
+            {
+                return x[0] - y[0];
+            }
+        }
+    }
+}
