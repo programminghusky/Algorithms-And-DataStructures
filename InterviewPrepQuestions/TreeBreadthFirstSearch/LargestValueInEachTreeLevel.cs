7f083117cc28e5725039701ac29dbf3cdd9d7705
programminghusky <minnu@DESKTOP-056E97D.localdomain>
2020-09-27 13:57:17 -0700

    Largest Value in each tree row

---
+++ InterviewPrepQuestions/TreeBreadthFirstSearch/LargestValueInEachTreeLevel.cs
@@ -0,0 +1,45 @@
+﻿/* Find the largest value on each level of a binary tree. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.TreeBreadthFirstSearch
+{
+    class LargestValueInEachTreeLevel
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            IList<int> listOfNodes = new List<int>();
+            if (root == null)
+                return listOfNodes;
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            nodes.Enqueue(root);
+            List<int> levelList;
+
+            while (nodes.Count != 0)
+            {
+                int length = nodes.Count;
+                levelList = new List<int>();
+                int maxValue = int.MinValue;
+                for (int i = 0; i < length; i++)
+                {
+                    TreeNode node = nodes.Dequeue();
+                    maxValue = Math.Max(maxValue, node.val);
+
+                    if (node.left != null)
+                    {
+                        nodes.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        nodes.Enqueue(node.right);
+                    }
+                }
+                listOfNodes.Add(maxValue);
+            }
+            return listOfNodes;
+        }
+    }
+}
