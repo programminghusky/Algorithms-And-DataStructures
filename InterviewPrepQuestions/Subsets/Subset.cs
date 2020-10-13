58f8cfcc65abdd8c1687fd289af62ad9acd66ef7
programminghusky <husky2020coding@gmail.com>
2020-10-12 23:05:28 -0700

    Generate all distinct subsets

---
+++ InterviewPrepQuestions/Subsets/Subset.cs
@@ -0,0 +1,32 @@
+﻿/* Given a set with distinct elements, find all of its distinct subsets */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.Subsets
+{
+    class Subset
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            IList<IList<int>> subsets = new List<IList<int>>();
+
+            //adding empty subset 
+            subsets.Add(new List<int>());
+
+            foreach(int curNum in nums)
+            {
+                int length = subsets.Count;
+                for(int i = 0; i< length ; i++)
+                {
+                    IList<int> set = new List<int>( subsets[i]);
+                    set.Add(curNum);
+                    subsets.Add(set);
+                }
+            }
+            return subsets;
+        }
+    }
+}
---
+++ InterviewPrepQuestions/TreeDepthFirstSearch/PathWithGivenSequence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.TreeDepthFirstSearch
+{
+    class PathWithGivenSequence
+    {
+        public bool IsValidSequence(TreeNode root, int[] arr)
+        {
+
+        }
+    }
+}
