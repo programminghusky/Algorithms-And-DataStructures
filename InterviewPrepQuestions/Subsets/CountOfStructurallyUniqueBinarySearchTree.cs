2e6c1b080b58c02e89008af1d8022e66fd6f4de4
programminghusky <husky2020coding@gmail.com>
2020-10-16 20:24:44 -0700

    Count of structurally unique Binary Search Trees that can store values from 1 to n

---
+++ InterviewPrepQuestions/Subsets/CountOfStructurallyUniqueBinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿/*Given a number ‘n’, write a function to return the count of structurally unique Binary Search Trees (BST) that can store
+ * values 1 to ‘n’. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrepQuestions.Subsets
+{
+    class CountOfStructurallyUniqueBinarySearchTree
+    {
+        public int NumTrees(int n)
+        {
+            if (n <= 1)
+                return 1;
+            int count = 0;
+            for(int i = 1; i<=n; i++)
+            {
+                int countOfleftSubTrees = NumTrees(i - 1);
+                int countOfRighSubTrees = NumTrees(n - i);
+                count += (countOfleftSubTrees * countOfRighSubTrees);
+            }
+            return count;
+        }
+    }
+}
